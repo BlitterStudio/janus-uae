adc956ee5b46769a173faacf15b1e8136541d21c
o1i <o1i@b46322d7-8905-45b6-b196-3a62c27b8adb>
2009-04-02 07:55:07 +0000

    update includes, update copyright

--- trunk/janus-uae/src/gfx-amigaos/ami-win.c
+++ trunk/janus-uae/src/gfx-amigaos/ami-win.c
@@ -1,18 +1,35 @@
-/*
-  * UAE - The Un*x Amiga Emulator
-  *
-  * Amiga interface
-  *
-  * Copyright 1996,1997,1998 Samuel Devulder.
-  * Copyright 2003-2007 Richard Drummond
-  */
+/************************************************************************ 
+ *
+ * Amiga interface
+ *
+ * Copyright 1996,1997,1998 Samuel Devulder.
+ * Copyright 2003-2007 Richard Drummond
+ * Copyright 2009 Oliver Brunner - aros<at>oliver-brunner.de
+ *
+ * This file is part of Janus-UAE.
+ *
+ * Janus-Daemon is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Janus-Daemon is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Janus-UAE. If not, see <http://www.gnu.org/licenses/>.
+ *
+ ************************************************************************/
 
 #include <gtk/gtk.h>
 #include "sysconfig.h"
 #include "sysdeps.h"
 
-
 #include "td-amigaos/thread.h"
+
+#include "od-amiga/j.h"
 
 /* sam: Argg!! Why did phase5 change the path to cybergraphics ? */
 //#define CGX_CGX_H <cybergraphics/cybergraphics.h>
@@ -114,7 +131,6 @@
 #include "picasso96.h"
 #undef BitMap
 
-#include "aroswin.h"
 /* uae includes are a mess..*/
 void uae_Signal(uaecptr task, uae_u32 mask);
 /****************************************************************************/
@@ -1874,8 +1890,6 @@ void graphics_notify_state (int state)
 /***************************************************************************
  * This stuff needs an own file, too 
  ***************************************************************************/
-
-#include "aroswin.h"
 
 UWORD get_LeftEdge(ULONG win) {
 
