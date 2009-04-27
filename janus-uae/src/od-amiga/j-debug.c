aa44a039853b2355a3023788989ae1959f16a2c3
o1i <o1i@b46322d7-8905-45b6-b196-3a62c27b8adb>
2009-04-27 13:40:35 +0000

    implemented debug for amigaos

---
+++ janus-uae/src/od-amiga/j-debug.c
@@ -0,0 +1,29 @@
+/************************************************************************ 
+ *
+ * Copyright 2009 Oliver Brunner - aros<at>oliver-brunner.de
+ *
+ * This file is part of Janus-UAE.
+ *
+ * Janus-Daemon is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Janus-Daemon is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Janus-UAE. If not, see <http://www.gnu.org/licenses/>.
+ *
+ ************************************************************************/
+
+#include "j.h"
+
+uae_u32 ad_debug(ULONG *m68k_results) {
+
+  kprintf("janusd: %s",(char *) get_real_address(m68k_results));
+
+  return TRUE;
+}
