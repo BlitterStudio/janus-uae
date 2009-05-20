2a014f765b6e0959f590516fdda30f8574ac6135
o1i <o1i@b46322d7-8905-45b6-b196-3a62c27b8adb>
2009-05-20 12:08:35 +0000

    Id test

--- janus-uae/src/gui-gtk/integration.c
+++ janus-uae/src/gui-gtk/integration.c
@@ -21,6 +21,8 @@
  * You should have received a copy of the GNU General Public License
  * along with Janus-UAE. If not, see <http://www.gnu.org/licenses/>.
  *
+ * $Id$
+ *
  ************************************************************************/
 
 #ifdef __AROS__
