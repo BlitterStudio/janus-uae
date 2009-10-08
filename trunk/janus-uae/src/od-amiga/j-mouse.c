bede3fcde9bbc1a18a7ee57392e674a6b2286e5b
o1i <o1i@b46322d7-8905-45b6-b196-3a62c27b8adb>
2009-10-08 13:20:09 +0000

    now seems to work (some bugs and much too much debug left)

--- trunk/janus-uae/src/od-amiga/j-mouse.c
+++ trunk/janus-uae/src/od-amiga/j-mouse.c
@@ -40,6 +40,8 @@ extern int thisframe_y_adjust;
  * SuperHires/LowRes scaling is done in the native part.
  *
  ***********************************************************************/
+#define BIT(n) (1<<n)
+
 static uae_u32 nonP96(struct Screen *screen, ULONG *m68k_results) {
   LONG x,y;
   LONG arosx, arosy;
@@ -55,10 +57,35 @@ static uae_u32 nonP96(struct Screen *screen, ULONG *m68k_results) {
   /* AROS mouse x coord to amigaOS x coord */
 
   //JWLOG("FOO: 0xdff08E: %x\n", diwstrt);
-  diwx=diwstrt & 0xb11111111;
-  //JWLOG("FOO: diwx:     %x\n", diwx);
-  diwhx=diwhigh & 0xb100000; /* H8 bit is nr 5 */
-  //JWLOG("FOO: diwhx:    %x\n", diwhx);
+
+  /* from the Hardware Reference Manual:
+   *
+   * Register Address  Write   Paula         Function
+   * -------- -------  -----   -------       --------
+   * DIWHIGH    1E4      W     A,D( E ) Display window -  upper bits for start, stop
+   * DIWSTOP    090      W       A      Display window stop (lower right vertical-horizontal position)
+   * DIWSTRT    08E      W       A      Display window start (upper left vertical-horizontal position)
+   * 
+   * These registers control display window size and position
+   * by locating the upper left and lower right corners.
+   * 
+   * BIT# 15,14,13,12,11,10,09,08,07,06,05,04,03,02,01,00
+   * -----------------------------------------------
+   * USE  V7 V6 V5 V4 V3 V2 V1 V0 H7 H6 H5 H4 H3 H2 H1 H0
+   * 
+   * DIWSTRT is vertically restricted to the upper 2/3
+   * of the display (V8=0) and horizontally restricted to
+   * the left 3/4 of the display (H8=0).
+   * 
+   * DIWSTOP is vertically restricted to the lower 1/2
+   * of the display (V8=/=V7) and horizontally restricted
+   * to the right 1/4 of the display (H8=1).
+   */
+
+  diwx=diwstrt  & 0xFF; /* Bits 0-7 */
+  JWLOG("FOO: diwx:     %x\n", diwx);
+  diwhx=diwhigh & BIT(5); /* H8 bit is nr 5 */
+  JWLOG("FOO: diwhx:    %x\n", diwhx);
   dx=diwx+diwhx;
 
   JWLOG("FOO: arosx:        %4d\n", arosx);
@@ -67,10 +94,13 @@ static uae_u32 nonP96(struct Screen *screen, ULONG *m68k_results) {
 
   JWLOG("FOO:               ----\n");
   JWLOG("FOO: arosx-dx:     %4d\n", x);
+
+#if 0
   x=x + 0x40; /* DISPLAY_LEFT_SHIFT */
   JWLOG("FOO:      +0x40:   %4d\n", 0x40);
   JWLOG("FOO:               ----\n");
   JWLOG("FOO x:             %4d\n", x);
+#endif
 
   x=x + (9*2) + 2; /* 2*DIW_DDF_OFFSET + HBLANK_OFFSET(=4) !? */
   JWLOG("FOO:    +2+9*2:    %4d\n", 22);
@@ -85,7 +115,7 @@ static uae_u32 nonP96(struct Screen *screen, ULONG *m68k_results) {
    * native part does this (janusd) 
    */
 
-  put_long(m68k_results, x); 
+  put_long_p(m68k_results, x); 
   //put_long_p(m68k_results, screen->MouseX); 
   
   JWLOG("FOO: ------------------------------\n");
@@ -94,7 +124,7 @@ static uae_u32 nonP96(struct Screen *screen, ULONG *m68k_results) {
   JWLOG("FOO: native2amiga_line_map[%3d]:  %4d\n", arosy, native2amiga_line_map[arosy]);
   JWLOG("FOO: thisframe_y_adjust:          %4d\n", thisframe_y_adjust);
   JWLOG("FOO: minfirstline:               -%4d\n", minfirstline);
-  JWLOG("FOO: minfirstline:               -  44\n");
+  JWLOG("FOO: (const)                     -%4d\n", 44);
   JWLOG("FOO:                             -----\n");
   y=-44 + coord_native_to_amiga_y(arosy) << 1;
   JWLOG("FOO:                              %4d\n", y);
@@ -103,11 +133,12 @@ static uae_u32 nonP96(struct Screen *screen, ULONG *m68k_results) {
   JWLOG("FOO:                             -----\n");
   JWLOG("FOO: res:                         %4d\n", y);
 
-  put_long(m68k_results+1, y); 
+  put_long_p(m68k_results+1, y); 
   //put_long_p(m68k_results+1, screen->MouseY); 
 
   return TRUE;
 }
+
 uae_u32 ad_job_get_mouse(ULONG *m68k_results) {
   struct Screen *screen;
   ULONG modeID;
@@ -145,9 +176,9 @@ uae_u32 ad_job_get_mouse(ULONG *m68k_results) {
     }
   }
 #endif
-  if(get_long(m68k_results) == 0) {
+  if(get_long_p(m68k_results) == 0) {
     JWLOG("no P96 screen \n");
-    nonP96(screen, m68k_results);
+    return nonP96(screen, m68k_results);
   }
 
   JWLOG("P96 screen \n");
