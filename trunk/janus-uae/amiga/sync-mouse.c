ea62c3741418412e489ae4bd72fdfe6afc4936e7
o1i <o1i@b46322d7-8905-45b6-b196-3a62c27b8adb>
2009-10-28 14:01:55 +0000

    WORKING (hopefully)

--- trunk/janus-uae/amiga/sync-mouse.c
+++ trunk/janus-uae/amiga/sync-mouse.c
@@ -355,7 +355,7 @@ void no_p96_fix_viewoffset(struct Screen *screen, WORD *x, WORD *y) {
 
   *x=*x - ((view->DxOffset -108)*2); /* 108? why, I don't know ;) */
 
-  *y=*y + view->DyOffset - 77;
+  *y=*y + view->DyOffset - 77; /* TODO */
 
 
   DebOut("no_p96_fix_viewoffset: x,y: %d, %d\n",*x,*y);
@@ -432,8 +432,8 @@ void sync_mouse() {
     DebOut("no_p96 =============================\n");
 
     no_p96_fix_viewoffset(screen, &x, &y);
-    //no_p96_fix_overscan  (screen, &x, &y);
-    //no_p96_fix_resolution(screen, &x, &y);
+    no_p96_fix_resolution(screen, &x, &y);
+    no_p96_fix_overscan  (screen, &x, &y);
 
     DebOut("no_96: x,y: %d, %d (after div)\n",x,y);
 
