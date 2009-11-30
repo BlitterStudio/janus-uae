89bdb6bd1e92efc3678d77ed6ad4106eba72c01d
o1i <o1i@b46322d7-8905-45b6-b196-3a62c27b8adb>
2009-11-30 13:29:27 +0000

    reduced debug

--- trunk/janus-uae/amiga/janus-daemon.h
+++ trunk/janus-uae/amiga/janus-daemon.h
@@ -97,10 +97,9 @@ char *public_screen_name(struct Screen *scr);
 /* debug.c */
 void DebOut(const char *format, ...);
 
-#if 0
 #define ENTER
 #define LEAVE
-#else
+#if 0
 #define ENTER DebOut("janusd: %s:%s entered\n",__FILE__,__func__);
 #define LEAVE DebOut("janusd: %s:%s left in line %d\n",__FILE__,__func__,__LINE__);
 #endif
