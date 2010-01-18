fa9359a528f06177eb2a8c2d2d185cd4d537da3c
o1i <o1i@b46322d7-8905-45b6-b196-3a62c27b8adb>
2010-01-18 15:49:52 +0000

    added j-launch.c

---
+++ janus-uae/src/od-amiga/j-launch.c
@@ -0,0 +1,29 @@
+/************************************************************************ 
+ *
+ * Copyright 2009 Oliver Brunner - aros<at>oliver-brunner.de
+ *
+ * This file is part of Janus-UAE.
+ *
+ * Janus-UAE is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Janus-UAE is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Janus-UAE. If not, see <http://www.gnu.org/licenses/>.
+ *
+ * $Id$
+ *
+ ************************************************************************/
+
+#include "j.h"
+
+uae_u32 ld_job_get(ULONG *m68k_results) {
+
+}
+    
