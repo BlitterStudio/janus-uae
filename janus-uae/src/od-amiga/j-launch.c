e2ca0511f8e82554175c216b95738f6775e677ba
o1i <o1i@b46322d7-8905-45b6-b196-3a62c27b8adb>
2010-01-19 15:56:52 +0000

    working for the first time

--- janus-uae/src/od-amiga/j-launch.c
+++ janus-uae/src/od-amiga/j-launch.c
@@ -23,7 +23,61 @@
 
 #include "j.h"
 
+/*m68k_results:
+ * status (LONG)
+ * position to path inside m68k_results (LONG)
+ * position to filename inside m68k_results (LONG)
+ * data (..)
+ */
+#define NR_LONGS 3
+
 uae_u32 ld_job_get(ULONG *m68k_results) {
 
+  char        *str_results;
+  char        *sep;
+  GSList      *first  =NULL;
+  JanusLaunch *jlaunch=NULL;
+
+  JWLOG("entered\n");
+
+  ObtainSemaphore(&sem_janus_launch_list);
+  if(janus_launch) {
+    first  =g_slist_nth(janus_launch, 0);
+    if(first) {
+      jlaunch=(struct JanusLaunch *) first->data;
+      janus_launch=g_slist_delete_link(janus_launch, first);
+    }
+  }
+  ReleaseSemaphore(&sem_janus_launch_list);
+
+  if(!jlaunch) {
+    JWLOG("jlaunch == NULL !?\n");
+    return;
+  }
+
+  JWLOG("found jlaunch %lx (%s)\n", jlaunch, jlaunch->amiga_path);
+
+  /* OK */
+  put_long_p(m68k_results, 1);
+
+  /* start position of path */
+  put_long_p(m68k_results+1, NR_LONGS*4);  
+
+  /* start of result strings */
+  str_results=(char *) get_real_address(m68k_results) + NR_LONGS*4 ;
+
+  /* TODO: check, if string fits!!!! */
+
+  /* copy full path */
+  strcpy(str_results, jlaunch->amiga_path);
+
+  /* seperate path and filename */
+  sep=PathPart(str_results);
+  sep[0]=(char) 0;
+
+  /* start of filename */
+  put_long_p(m68k_results+2, NR_LONGS*4 + strlen(str_results) + 1);  
+
+  return 1;
 }
     
