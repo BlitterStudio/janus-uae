528f73f6f2f36c9db95c3a3832f429b7785a6ef1
o1i <o1i@b46322d7-8905-45b6-b196-3a62c27b8adb>
2010-01-21 13:50:45 +0000

    added argument support, still buggy and a lot of debug

--- janus-uae/src/od-amiga/j-launch.c
+++ janus-uae/src/od-amiga/j-launch.c
@@ -28,15 +28,22 @@
  * position to path inside m68k_results (LONG)
  * position to filename inside m68k_results (LONG)
  * data (..)
+ *
+ * attention: jlaunch->args can be NULL in case there are no args,
+ *            so every acces to jlaunch->args[] crashes
  */
-#define NR_LONGS 3
+#define NR_LONGS 4 /* status, offset of path, offset of filename and terminating 0 */
 
 uae_u32 ld_job_get(ULONG *m68k_results) {
 
   char        *str_results;
+  char        *results;
   char        *sep;
   GSList      *first  =NULL;
   JanusLaunch *jlaunch=NULL;
+  ULONG        nr_args;
+  ULONG        i;
+  ULONG        target;
 
   JWLOG("entered\n");
 
@@ -44,7 +51,7 @@ uae_u32 ld_job_get(ULONG *m68k_results) {
   if(janus_launch) {
     first  =g_slist_nth(janus_launch, 0);
     if(first) {
-      jlaunch=(struct JanusLaunch *) first->data;
+      jlaunch=(JanusLaunch *) first->data;
       janus_launch=g_slist_delete_link(janus_launch, first);
     }
   }
@@ -52,31 +59,78 @@ uae_u32 ld_job_get(ULONG *m68k_results) {
 
   if(!jlaunch) {
     JWLOG("jlaunch == NULL !?\n");
-    return;
+    return 0;
   }
 
   JWLOG("found jlaunch %lx (%s)\n", jlaunch, jlaunch->amiga_path);
 
-  /* OK */
+  /* count nr_args */
+  nr_args=0;
+  if(jlaunch->args) {
+    while(jlaunch->args[nr_args]) {
+      nr_args++;
+    }
+  }
+
+  JWLOG("nr_args: %d\n", nr_args);
+
+  /* status: OK */
   put_long_p(m68k_results, 1);
 
   /* start position of path */
-  put_long_p(m68k_results+1, NR_LONGS*4);  
+  put_long_p(m68k_results+1, (nr_args+NR_LONGS)*4);  
+  JWLOG("DEBOUT: m68k_results[%d]=%d (%lx)\n", 1, get_long(m68k_results+1), get_long(m68k_results+1));
+
+  results=(char *) get_real_address((LONG) m68k_results);
 
   /* start of result strings */
-  str_results=(char *) get_real_address(m68k_results) + NR_LONGS*4 ;
+  str_results=results + (nr_args+NR_LONGS)*4 ;
 
   /* TODO: check, if string fits!!!! */
 
   /* copy full path */
   strcpy(str_results, jlaunch->amiga_path);
+  JWLOG("str_results: %s\n", str_results);
 
   /* seperate path and filename */
   sep=PathPart(str_results);
   sep[0]=(char) 0;
+  JWLOG("path: %s\n", str_results);
 
   /* start of filename */
-  put_long_p(m68k_results+2, NR_LONGS*4 + strlen(str_results) + 1);  
+  put_long_p(m68k_results+2, (nr_args+NR_LONGS)*4 + strlen(str_results) + 1);  
+  JWLOG("DEBOUT: m68k_results[%d]=%d (%lx)\n", 2, get_long(m68k_results+2), get_long(m68k_results+2));
+  JWLOG("DEBOUT: --\n");
+  JWLOG("filename: %s\n", results + (nr_args+NR_LONGS)*4 + strlen(str_results) + 1);
+
+  /* copy arguments */
+
+  /* there will be the first string of our arguments, if we have one */
+  target=(nr_args+NR_LONGS)*4 + strlen(jlaunch->amiga_path) + 1;
+
+  /* copy all args */
+  for(i=0; jlaunch->args && (jlaunch->args[i]!=NULL); i++) {
+
+    JWLOG("arg #%d target %d (%s)\n", i, target, jlaunch->args[i]);
+
+    /* store offset */
+    put_long_p(m68k_results+NR_LONGS-1+i, target); /* -1 is the count of the terminating 0 */
+    /* copy string */
+    strcpy(results+target, jlaunch->args[i]);
+
+    target=target+strlen(jlaunch->args[i]+1);
+    if(target > 8000) {
+      JWLOG("ERROR: too many aruments!!\n");
+    }
+
+  }
+  //put_long_p(m68k_results+NR_LONGS+i, 0); /* terminate*/
+
+  for(i=0; i<50; i++) {
+    JWLOG("DEBOUT: m68k_results[%d]=%d (%lx)\n", i, get_long(m68k_results+i), get_long(m68k_results+i));
+  }
+
+  /* TODO: free jlaunch stuff here! */
 
   return 1;
 }
