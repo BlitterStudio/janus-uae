d0916aebf260cc005130169e4f80f211c7699162
o1i <o1i@b46322d7-8905-45b6-b196-3a62c27b8adb>
2010-02-11 10:00:43 +0000

    bsdsocket.library can now be built. host dependent part is completely missing though.

--- trunk/janus-uae/src/include/bsdsocket.h
+++ trunk/janus-uae/src/include/bsdsocket.h
@@ -1,11 +1,30 @@
- /*
-  * UAE - The Un*x Amiga Emulator
-  *
-  * bsdsocket.library emulation
-  *
-  * Copyright 1997,98 Mathias Ortmann
-  *
-  */
+/************************************************************************ 
+ *
+ * bsdsocket.library emulation
+ *
+ * Copyright 1997,98 Mathias Ortmann
+ * Copyright 2009 Oliver Brunner - aros<at>oliver-brunner.de
+ *
+ * This file is part of Janus-UAE.
+ *
+ * Janus-UAE is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Janus-UAE is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Janus-UAE. If not, see <http://www.gnu.org/licenses/>.
+ *
+ * This file is based on a parts of cbio.c, (c) 1992 Commodore-Amiga.
+ *
+ * $Id$
+ *
+ ************************************************************************/
 
 //#define TRACING_ENABLED
 
