00c840ae78cd046e44d9be13fd8c1d535e7c32ae
o1i <o1i@b46322d7-8905-45b6-b196-3a62c27b8adb>
2010-10-26 14:28:48 +0000

    made debug optional

--- janus-uae/src/od-amiga/j-debug.c
+++ janus-uae/src/od-amiga/j-debug.c
@@ -4,12 +4,12 @@
  *
  * This file is part of Janus-UAE.
  *
- * Janus-Daemon is free software: you can redistribute it and/or modify
+ * Janus-UAE is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
  *
- * Janus-Daemon is distributed in the hope that it will be useful,
+ * Janus-UAE is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
@@ -17,13 +17,21 @@
  * You should have received a copy of the GNU General Public License
  * along with Janus-UAE. If not, see <http://www.gnu.org/licenses/>.
  *
+ * $Id$
+ *
+ * Helper function to write to AROS console from the amigaOS side.
+ *
  ************************************************************************/
 
 #include "j.h"
 
+//#define M68K_DEBUG 1
+
 uae_u32 ad_debug(ULONG *m68k_results) {
 
+#if M68K_DEBUG
   kprintf("m68k/%s",(char *) get_real_address(m68k_results));
+#endif
 
   return TRUE;
 }
