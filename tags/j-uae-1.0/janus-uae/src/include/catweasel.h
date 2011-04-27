6c7335cbaa53ee6347da220b5fea96edb2eb370f
o1i <o1i@b46322d7-8905-45b6-b196-3a62c27b8adb>
2011-04-27 08:38:51 +0000

    v1.0 tag

---
+++ tags/j-uae-1.0/janus-uae/src/include/catweasel.h
@@ -0,0 +1,158 @@
+#ifndef __CATWEASEL_H__
+#define __CATWEASEL_H__
+
+
+#ifdef CATWEASEL
+
+extern struct catweasel_contr cwc;
+extern int catweasel_read_keyboard (uae_u8 *keycode);
+extern int catweasel_init (void);
+extern void catweasel_free (void);
+extern uae_u32 catweasel_do_bget (uaecptr addr);
+extern void catweasel_do_bput (uaecptr addr, uae_u32 b);
+extern int catweasel_read_joystick (int stick, uae_u8 *dir, uae_u8 *buttons);
+extern void catweasel_hsync (void);
+
+typedef struct catweasel_drive {
+    struct catweasel_contr *contr; /* The controller this drive belongs to */
+    int number;                    /* Drive number: 0 or 1 */
+    int type;                      /* 0 = not present, 1 = 3.5" */
+    int track;                     /* current r/w head position (0..79) */
+    int diskindrive;               /* 0 = no disk, 1 = disk in drive */
+    int wprot;                     /* 0 = not, 1 = write protected */
+    unsigned char sel;
+    unsigned char mot;
+} catweasel_drive;
+
+typedef struct catweasel_contr {
+    int type;                      /* see CATWEASEL_TYPE_* defines below */
+    int direct_access;
+    int iobase;                    /* 0 = not present (factory default is 0x320) */
+    void (*msdelay)(int ms);       /* microseconds delay routine, provided by host program */
+    catweasel_drive drives[2];     /* at most two drives on each controller */
+    int control_register;          /* contents of control register */
+    unsigned char crm_sel0;        /* bit masks for the control / status register */
+    unsigned char crm_sel1;
+    unsigned char crm_mot0;
+    unsigned char crm_mot1;
+    unsigned char crm_dir;
+    unsigned char crm_step;
+    unsigned char srm_trk0;
+    unsigned char srm_dchg;
+    unsigned char srm_writ;
+    unsigned char srm_dskready;
+    int io_sr;                     /* IO port of control / status register */
+    int io_mem;                    /* IO port of memory register */
+    int sid[2];
+    int can_sid, can_mouse, can_joy, can_kb;
+} catweasel_contr;
+
+#define CATWEASEL_TYPE_NONE  -1
+#define CATWEASEL_TYPE_MK1    1
+#define CATWEASEL_TYPE_MK3    3
+#define CATWEASEL_TYPE_MK4    4
+
+/* Initialize a Catweasel controller; c->iobase and c->msdelay must have
+   been initialized -- msdelay might be used */
+void catweasel_init_controller(catweasel_contr *c);
+
+/* Reset the controller */
+void catweasel_free_controller(catweasel_contr *c);
+
+/* Set current drive select mask */
+void catweasel_select(catweasel_contr *c, int dr0, int dr1);
+
+/* Start/stop the drive's motor */
+void catweasel_set_motor(catweasel_drive *d, int on);
+
+/* Move the r/w head */
+int catweasel_step(catweasel_drive *d, int dir);
+
+/* Check for a disk change and update d->diskindrive
+   -- msdelay might be used. Returns 1 == disk has been changed */
+int catweasel_disk_changed(catweasel_drive *d);
+
+/* Check if disk in selected drive is write protected. */
+int catweasel_write_protected(catweasel_drive *d);
+
+/* Read data -- msdelay will be used */
+int catweasel_read(catweasel_drive *d, int side, int clock, int time);
+
+int catweasel_read_mouse (int port, int *dx, int *dy, int *buttons);
+
+/* Write data -- msdelay will be used. If time == -1, the write will
+   be started at the index pulse and stopped at the next index pulse,
+   or earlier if the Catweasel RAM contains a 128 end byte.  The
+   function returns after the write has finished. */
+int catweasel_write(catweasel_drive *d, int side, int clock, int time);
+
+int catweasel_fillmfm (catweasel_drive *d, uae_u16 *mfm, int side, int clock, int rawmode);
+
+int catweasel_diskready(catweasel_drive *d);
+int catweasel_track0(catweasel_drive *d);
+#define CW_VENDOR       0xe159
+#define CW_DEVICE       0x0001
+#define CW_MK3_SUBVENDOR    0x1212
+#define CW_MK3_SUBDEVICE    0x0002
+#define CW_MK4_SUBVENDOR1   0x5213
+#define CW_MK4_SUBVENDOR2   0x5200
+#define CW_MK4_SUBDEVICE1   0x0002
+#define CW_MK4_SUBDEVICE2   0x0003
+
+// generic registers 
+#define CW_DATA_DIRECTION   0x02
+#define CW_SELECT_BANK      0x03
+#define CW_PORT_OUT_DIR     CW_SELECT_BANK
+#define CW_PORT_AUX     0x05
+#define CW_PORT_IN_DIR      0x07
+
+// values for CW_SELECT_BANK 
+#define CW_BANK_RESETFPGA   0x00
+#define CW_BANK_FIFO        0x01
+#define CW_BANK_FLOPPY      0x41
+#define CW_BANK_IO      0x81
+#define CW_BANK_IRQ     0xC1
+
+// registers in FLOPPY bank 
+#define CW_FLOPPY_JOYDAT    0xC0
+#define CW_FLOPPY_JOYBUT    0xC8
+#define CW_FLOPPY_JOYBUT_DIR    0xCC
+#define CW_FLOPPY_KEYDAT    0xD0
+#define CW_FLOPPY_KEYSTATUS 0xD4
+#define CW_FLOPPY_SIDDAT    0xD8
+#define CW_FLOPPY_SIDADDR   0xDC
+#define CW_FLOPPY_MEMORY    0xE0
+#define CW_FLOPPY_RESETPOINTER  0xE4
+#define CW_FLOPPY_CONTROL   0xE8
+#define CW_FLOPPY_OPTION    0xEC
+#define CW_FLOPPY_START_A   0xF0
+#define CW_FLOPPY_START_B   0xF4
+#define CW_FLOPPY_IRQ       0xFC
+
+// registers in IO bank 
+#define CW_IO_MOUSEY1       0xC0
+#define CW_IO_MOUSEX1       0xC4
+#define CW_IO_MOUSEY2       0xC8
+#define CW_IO_MOUSEX2       0xCC
+#define CW_IO_BUTTON        0xD0
+
+// registers in CW_BANK_IRQ 
+#define CW_IRQ_R0       0xC0
+#define CW_IRQ_R1       0xC4
+#define CW_IRQ_M0       0xC8
+#define CW_IRQ_M1       0xCC
+// bits in registers in CW_BANK_IRQ 
+#define CW_IRQ_MK3FLOPPY    0x01
+#define CW_IRQ_INDEX        0x02
+#define CW_IRQ_FLOPPY_START 0x04
+#define CW_IRQ_FLOPPY_END   0x08
+#define CW_IRQ_SID_FIFO_HALF_EMPTY  0x10
+#define CW_IRQ_SID_READ     0x20
+#define CW_IRQ_MEM_EQUAL    0x40
+#define CW_IRQ_KEYBOARD     0x80
+#define CW_IRQ_SID_FIFO_EMPTY   0x01
+#define CW_IRQ_SID_FEEDBACK 0x02
+
+#endif
+
+#endif
---
+++ tags/j-uae-1.0/janus-uae/src/od-amiga/j-dispatch.c
@@ -0,0 +1,571 @@
+/************************************************************************ 
+ *
+ * Copyright 2009 Oliver Brunner - aros<at>oliver-brunner.de
+ *
+ * This file is part of Janus-UAE.
+ *
+ * Janus-UAE is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Janus-UAE is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Janus-UAE. If not, see <http://www.gnu.org/licenses/>.
+ *
+ * $Id$
+ *
+ ************************************************************************/
+
+#include <exec/exec.h>
+#include <dos/dos.h>
+#include <dos/dostags.h>
+#include <proto/dos.h>
+
+//#define JWTRACING_ENABLED 1
+#include "j.h"
+
+BOOL init_done=FALSE;
+
+/* this is disabled! TODO! */
+struct Window *assert_window (struct Window *search) {
+
+  return search;
+#if 0
+  struct Window *win;
+  struct Screen *screen;
+
+  screen=LockPubScreen(NULL);
+  win=screen->FirstWindow;
+  UnlockPubScreen(NULL,screen);
+
+  while(win) {
+    if(search==win) {
+      return win;
+    }
+    win=win->NextWindow;
+  }
+
+  JWLOG("assert_window(%lx) failed\n",search);
+
+  return NULL;
+#endif
+}
+
+gint aos3_process_compare(gconstpointer aos3win, gconstpointer t) {
+
+  if(((JanusWin *)aos3win)->task == t) {
+    return 0;
+  }
+  return 1; /* don't care for sorting here */
+}
+
+gint aos3_screen_process_compare(gconstpointer jscreen, gconstpointer t) {
+
+  if(((JanusScreen *)jscreen)->task == t) {
+    return 0;
+  }
+  return 1; /* don't care for sorting here */
+}
+
+
+gint aos3_window_compare(gconstpointer aos3win, gconstpointer w) {
+
+  if(((JanusWin *)aos3win)->aos3win == w) {
+    return 0;
+  }
+  return 1; /* don't care for sorting here */
+}
+
+gint aros_window_compare(gconstpointer jwin, gconstpointer aroswin) {
+
+  if(((JanusWin *)jwin)->aroswin == aroswin) {
+    return 0;
+  }
+  return 1; /* don't care for sorting here */
+}
+
+gint aos3_screen_compare(gconstpointer jscreen, gconstpointer s) {
+
+  //JWLOG("screen: %lx == %lx ?\n",((JanusScreen *)jscreen)->aos3screen,s);
+
+  if(((JanusScreen *)jscreen)->aos3screen == s) {
+    return 0;
+  }
+  return 1; /* don't care for sorting here */
+}
+
+#define GET_JOB_RESULTSIZE 32*4
+
+/*********************************************************
+ *
+ * The GET_JOB magic (how it should work)
+ * 
+ * It is not trivial (at least not for me) to call
+ * amigaOS functions from the x86 uae code. The other
+ * way is working without problems with the help of
+ * emulator traps.
+ *
+ * So to call 68k functions, you just place a "job"
+ * in the queue, which a 68k-slave process (aros-daemon)
+ * pulls.
+ * The Slave then tries to do the job.
+ *
+ * with uae_Signal you can easily send a signal
+ * to an aos3 task (our daemon). At setup, the
+ * daemon told us his task and his selected signal.
+ *
+ * A emulator trap causes aroshack_helper to
+ * be run. If D0 == GET_JOBS, D1 decides,
+ * which job to do.
+ *
+ * D0: primary group (GET_JOB, ..)
+ * D1: secondary group (GET_JOB_RESIZE, ..)
+ * A0: memory block for parameters/results
+ *
+ *********************************************************/
+
+WORD get_lo_word(ULONG *field) {
+  ULONG l;
+
+  l=get_long_p(field);
+  l=l & 0xFFFF;
+  return (WORD) l;
+}
+
+WORD get_hi_word(ULONG *field) {
+  ULONG l;
+
+  l=get_long_p(field);
+  l=l & 0xFFFF0000;
+  l=l/0x10000;
+  return (WORD) l;
+}
+
+/**********************************************************
+ * ad_job_fetch_message
+ *
+ * This is the aros-daemon calling us, to get his next
+ * message. if we put a 0 in the first long of the results,
+ * no messages are waiting.
+ *
+ * Only return one message per call, the janus-daemon
+ * will keep calling, as long as he gets messages
+ **********************************************************/
+int wait=0;
+static uae_u32 ad_job_fetch_message(ULONG *m68k_results) {
+  JanusMsg *j;
+
+  ObtainSemaphore(&janus_messages_access);
+
+  /* check, if we already sent that messages */
+  if(janus_messages) {
+    j=(JanusMsg *) janus_messages->data;
+    if(j->old) {
+      janus_messages=g_slist_remove_link(janus_messages, janus_messages);
+      //janus_messages=g_list_remove(janus_messages, j);
+      FreeVec(j);
+    }
+  }
+
+  /* send new message and mark it old */
+  if(janus_messages) {
+    j=(JanusMsg *) janus_messages->data;
+    //j=(JanusMsg *) g_slist_nth_data(janus_messages, 0);
+    put_long((ULONG) (m68k_results    ), (ULONG) j->type);
+    put_long((ULONG) (m68k_results + 1), (ULONG) j->jwin->aos3win);
+    j->old=TRUE;
+  }
+  else {
+    put_long_p(m68k_results,0); /* nothing to do */
+  }
+
+  ReleaseSemaphore(&janus_messages_access);
+
+  return TRUE;
+}
+
+//int coord_native_to_amiga_x (int x);
+int coord_native_to_amiga_y (int y);
+
+extern int diwstrt, diwhigh;
+extern int *native2amiga_line_map;
+extern int thisframe_y_adjust;
+
+static uae_u32 ad_test(ULONG *m68k_results) {
+
+  JWLOG("\nAD_TEST AD_TEST AD_TEST\n\n");
+
+  return TRUE;
+}
+
+void put_long_p(ULONG *p, ULONG value) {
+  put_long((ULONG) p, value);
+}
+
+ULONG get_long_p(ULONG *p) {
+  return (ULONG) get_long((uaecptr) p);
+}
+
+UWORD get_word_p(ULONG *p) {
+  return (UWORD) get_word((uaecptr) p);
+}
+
+void unlock_jgui(void);
+
+/*********************************
+ * setup janusd
+ *********************************/
+static uae_u32 jd_setup(TrapContext *context, ULONG *param) {
+    ULONG want_to_die;
+    /* want_to_die:
+     *   0: ignore the value, do nothing, just fetch the prefs setting
+     *   1: daemon wants to quit
+     *   2: demon wants to run again
+     */
+
+    //JWLOG("jd_setup(.., task %lx, .., stop %d)\n",get_long_p(param),get_long_p(param+8));
+#if 0
+    JWLOG("::::::::::::::AD_SETUP::::::::::::::::::::::::\n");
+    JWLOG("AD__MAXMEM: %d\n", m68k_dreg(&context->regs, 1));
+    JWLOG("param:      %lx\n",m68k_areg(&context->regs, 0));
+    JWLOG("Task:       %lx\n",get_long_p(param  ));
+    JWLOG("Signal:     %lx\n",get_long_p(param+4));
+    JWLOG("Stop:       %d\n", get_long_p(param+8));
+#endif
+
+    if(!init_done) {
+      JWLOG("AD_SETUP called first time => InitSemaphore etc\n");
+
+      /* from now on (aos3_task && aos3_task_signal) the
+       * aos3 aros-daemon is ready to take orders!
+       */
+
+      aos3_task=get_long_p(param);
+      aos3_task_signal=get_long_p(param+4);
+
+      InitSemaphore(&sem_janus_window_list);
+      InitSemaphore(&sem_janus_screen_list);
+      InitSemaphore(&aos3_thread_start);
+      InitSemaphore(&janus_messages_access);
+      InitSemaphore(&sem_janus_active_win);
+      InitSemaphore(&sem_janus_active_custom_screen);
+      InitSemaphore(&sem_janus_access_W);
+      InitSemaphore(&sem_janus_win_handling);
+      init_done=TRUE;
+
+      unlock_jgui();
+
+    }
+
+    want_to_die=get_long_p(param+8);
+
+    if(want_to_die == 1) {
+      JWLOG("jdaemon tells us, he wants to die (received a SIG-C)\n");
+      changed_prefs.jcoherence=FALSE;
+      close_all_janus_windows();
+      close_all_janus_screens();
+
+      /* update gui !! */
+    }
+
+    if(want_to_die == 2) {
+      JWLOG("jdaemon tells us, he wants to live again (received a SIG-D)\n");
+      changed_prefs.jcoherence=TRUE;
+      /* update gui !! */
+    }
+
+    //JWLOG("return %d\n", changed_prefs.jcoherence);
+    put_long_p(param+8, changed_prefs.jcoherence);
+    return changed_prefs.jcoherence;
+}
+
+/*********************************
+ * setup clipd
+ *********************************/
+static uae_u32 cd_setup(TrapContext *context, ULONG *param) {
+    ULONG want_to_die;
+    /* want_to_die:
+     *   0: ignore the value, do nothing, just fetch the prefs setting
+     *   1: daemon wants to quit
+     *   2: demon wants to run again
+     */
+
+    JWLOG("cd_setup(.., task %lx, .., stop %d)\n",get_long_p(param),get_long_p(param+12));
+
+    if(!aos3_clip_task) {
+      JWLOG("AD_CLIP_SETUP called first time => Init..\n");
+
+      /* from now on (aos3_clip_task && aos3_clip_signal) the
+       * aos3 clipd is ready to take orders!
+       */
+
+      aos3_clip_task=get_long_p(param);
+      aos3_clip_signal=get_long_p(param+4);
+      aos3_clip_to_amigaos_signal=get_long_p(param+8);
+
+      clipboard_hook_install();
+
+      unlock_jgui();
+    }
+
+    want_to_die=get_long_p(param+12);
+    JWLOG("want_to_die:%d\n",want_to_die);
+
+    if(want_to_die == 1) {
+      JWLOG("clipd tells us, he wants to die (received a SIG-C)\n");
+      changed_prefs.jclipboard=FALSE;
+
+      /* update gui !! */
+    }
+
+    if(want_to_die == 2) {
+      JWLOG("clipd tells us, he wants to live again (received a SIG-D)\n");
+      changed_prefs.jclipboard=TRUE;
+      /* update gui !! */
+    }
+
+    JWLOG("return %d\n", changed_prefs.jclipboard);
+    put_long_p(param+12, changed_prefs.jclipboard);
+    return changed_prefs.jclipboard;
+}
+
+/*********************************
+ * setup launchd
+ *********************************/
+static uae_u32 ld_setup(TrapContext *context, ULONG *param) {
+    ULONG want_to_die;
+    /* want_to_die:
+     *   0: ignore the value, do nothing, just fetch the prefs setting
+     *   1: daemon wants to quit
+     *   2: demon wants to run again
+     */
+
+    JWLOG("ld_setup(.., task %lx, .., stop %d)\n",get_long_p(param),get_long_p(param+12));
+
+    if(!aos3_launch_task) {
+      JWLOG("AD_LAUNCH_SETUP called first time => Init..\n");
+
+      /* from now on (aos3_launch_task && aos3_launch_signal) the
+       * aos3 launchd is ready to take orders!
+       */
+
+      InitSemaphore(&sem_janus_launch_list);
+
+      aos3_launch_task=get_long_p(param);
+      aos3_launch_signal=get_long_p(param+4);
+
+      unlock_jgui();
+
+      aros_cli_start_thread();
+    }
+
+    want_to_die=get_long_p(param+12);
+    JWLOG("want_to_die:%d\n",want_to_die);
+
+    if(want_to_die == 1) {
+      JWLOG("launchd tells us, he wants to die (received a SIG-C)\n");
+      //TODO changed_prefs.jclipboard=FALSE;
+
+      /* update gui !! */
+    }
+
+    if(want_to_die == 2) {
+      JWLOG("clipd tells us, he wants to live again (received a SIG-D)\n");
+      //TODO changed_prefs.jclipboard=TRUE;
+      /* update gui !! */
+    }
+
+    JWLOG("return %d\n", changed_prefs.jclipboard);
+    put_long_p(param+12, changed_prefs.jclipboard);
+    // TODO
+    return changed_prefs.jclipboard;
+}
+
+/**********************************************************
+ * this stuff gets called from janusd/clipd 
+ **********************************************************/
+uae_u32 REGPARAM2 aroshack_helper (TrapContext *context) {
+
+  unsigned long service;
+  char *source;
+  char *windowtitle;
+  char c;
+  int i=0;
+  APTR m68k_bm;
+  struct BitMap *aos_bm;
+  struct RastPort *rport;
+  struct Task *clone_task;
+
+  service = m68k_dreg (&context->regs, 0);
+
+  //JWLOG("aroshack_helper: service %d\n",service);
+
+  //t=get_long(m68k_dreg (&context->regs, 1));
+  
+  switch(service) {
+
+    case AD_SETUP: {
+      /* the janusd gets ready to serve */
+      ULONG *param= (ULONG *) m68k_areg(&context->regs, 0);
+
+      return jd_setup(context, param);
+    };
+
+    case AD_CLIP_SETUP: {
+      /* the clipd gets ready to serve */
+      ULONG *param= (ULONG *) m68k_areg(&context->regs, 0);
+
+      return cd_setup(context, param);
+    };
+
+    case AD_LAUNCH_SETUP: {
+      /* the launchd gets ready to serve */
+      ULONG *param= (ULONG *) m68k_areg(&context->regs, 0);
+
+      return ld_setup(context, param);
+    };
+
+    case AD_SHUTDOWN: {
+
+      /* there is *no* way, to stop a daemon, as they
+       * patch the aos3 system and those patches are *not safe*
+       * to remove
+       */
+      printf("ERROR: DO NOT USE AD_SHUTDOWN!!\n");
+      JWLOG ("ERROR: DO NOT USE AD_SHUTDOWN!!\n");
+
+/*
+      aos3_task=0;
+      aos3_task_signal=0;
+*/
+
+      return FALSE;
+      break;
+    };
+
+    /* the aros_daemon wants its orders*/
+    case AD_GET_JOB: {
+      ULONG job=m68k_dreg(&context->regs, 1);
+      ULONG *m68k_results= (ULONG *) m68k_areg(&context->regs, 0);
+
+      JWLOG("AD_GET_JOB: %d\n", job);
+
+      switch(job) {
+    	case AD_GET_JOB_LIST_WINDOWS: 
+       	  return ad_job_update_janus_windows(m68k_results);
+    	case AD_TEST: 
+       	  return ad_test(m68k_results);
+    	case AD_GET_JOB_DEBUG: 
+       	  return ad_debug(m68k_results);
+    	case AD_GET_JOB_SYNC_WINDOWS: 
+       	  return ad_job_sync_windows(m68k_results);
+    	case AD_GET_JOB_REPORT_UAE_WINDOWS: 
+       	  return ad_job_report_uae_windows(m68k_results);
+    	case AD_GET_JOB_REPORT_HOST_WINDOWS: 
+       	  return ad_job_report_host_windows(m68k_results);
+    	case AD_GET_JOB_MARK_WINDOW_DEAD: 
+       	  return ad_job_mark_window_dead((ULONG) m68k_results);
+    	case AD_GET_JOB_GET_MOUSE: 
+       	  return ad_job_get_mouse(m68k_results);
+    	case AD_GET_JOB_SWITCH_UAE_WINDOW: 
+       	  return ad_job_switch_uae_window(m68k_results);
+    	case AD_GET_JOB_ACTIVE_WINDOW: 
+       	  return ad_job_active_window(m68k_results);
+    	case AD_GET_JOB_NEW_WINDOW: 
+       	  return ad_job_new_window((ULONG) m68k_results);
+    	case AD_GET_JOB_LIST_SCREENS: 
+       	  return ad_job_list_screens(m68k_results);
+    	case AD_GET_JOB_MESSAGES: 
+       	  return ad_job_fetch_message(m68k_results);
+    	case AD_GET_JOB_OPEN_CUSTOM_SCREEN: 
+       	  return ad_job_open_custom_screen(m68k_results);
+    	case AD_GET_JOB_CLOSE_SCREEN: 
+       	  return ad_job_close_screen(m68k_results);
+    	case AD_GET_JOB_TOP_SCREEN: 
+       	  return ad_job_top_screen(m68k_results);
+	case AD_GET_JOB_SCREEN_DEPTH:
+       	  return ad_job_screen_depth((ULONG) m68k_areg(&context->regs, 0), 
+	                             (ULONG) m68k_dreg(&context->regs, 3));
+	case AD_GET_JOB_MODIFY_IDCMP:
+	  JWLOG("AD_GET_JOB_MODIFY_IDCMP(%lx,%d)\n",(ULONG) m68k_areg(&context->regs, 0),  (ULONG) m68k_dreg(&context->regs, 3));
+	  return TRUE;
+	case AD_GET_JOB_UPDATE_GADGETS:
+	  JWLOG("aroshack_helper: job: AD_GET_JOB_UPDATE_GADGETS\n");
+       	  return ad_job_update_gadgets((ULONG) m68k_areg(&context->regs, 3));
+	case AD_GET_JOB_SET_WINDOW_TITLES:
+       	  return ad_job_set_window_titles((ULONG) m68k_areg(&context->regs, 0));
+	case AD_GET_JOB_WINDOW_LIMITS:
+       	  return ad_job_window_limits( (ULONG) m68k_areg(&context->regs, 0),
+	                               ( WORD) m68k_dreg(&context->regs, 2),
+	                               ( WORD) m68k_dreg(&context->regs, 3),
+	                               (UWORD) m68k_dreg(&context->regs, 4),
+	                               (UWORD) m68k_dreg(&context->regs, 5) );
+
+	default:
+	  JWLOG("ERROR!! aroshack_helper: unkown job: %d\n",m68k_dreg(&context->regs, 1));
+	  return FALSE;
+      }
+      case AD_CLIP_JOB: {
+	ULONG job=m68k_dreg(&context->regs, 1);
+	ULONG *m68k_results= (ULONG *) m68k_areg(&context->regs, 0);
+	switch(job) {
+	  case JD_AMIGA_CHANGED:
+	    /* remember clipboard change in amigaOS */
+	    if(changed_prefs.jclipboard) {
+	      clipboard_amiga_changed=get_long_p(m68k_results);
+	      JWLOG("JD_AMIGA_CHANGED: clipboard_amiga_changed %d\n", clipboard_amiga_changed);
+	      put_long((ULONG) (m68k_results), (ULONG) TRUE);
+	    }
+	    else {
+	      /* disabled */
+	      JWLOG("JD_CLIP_COPY_TO_AROS: clipboard disabled\n");
+	      put_long((ULONG) (m68k_results), (ULONG) FALSE);
+	    }
+	    return TRUE;
+	  case JD_CLIP_COPY_TO_AROS:
+	    if(changed_prefs.jclipboard) {
+	      /* clipd told us to take the amigaOS clipboard and copy it to AROS clipboard */
+	      copy_clipboard_to_aros_real(get_long(m68k_results + 4), get_long(m68k_results + 8));
+	      JWLOG("copy_clipboard_to_aros_real returned\n");
+	    }
+	    else {
+	      JWLOG("JD_CLIP_COPY_TO_AROS: clipboard disabled\n");
+	    }
+	    return TRUE;
+	  case JD_CLIP_GET_AROS_LEN:
+	    /* get clipboard len */
+	    put_long((ULONG) (m68k_results), aros_clipboard_len());
+	    return TRUE;
+	  case JD_CLIP_COPY_FROM_AROS:
+	    copy_clipboard_to_amigaos_real(get_long_p(m68k_results), get_long_p(m68k_results+4));
+	    return TRUE;
+  	  default:
+  	    JWLOG("ERROR!! CLIP_JOB: unkown job: %d\n",m68k_dreg(&context->regs, 1));
+  	    return FALSE;
+	}
+      }
+      case AD_LAUNCH_JOB: {
+	ULONG job=m68k_dreg(&context->regs, 1);
+	ULONG *m68k_results= (ULONG *) m68k_areg(&context->regs, 0);
+	switch(job) {
+	  case LD_GET_JOB:
+	    return ld_job_get(m68k_results);
+	    break;
+  	  default:
+  	    JWLOG("ERROR!! LAUCH_JOB: unkown job: %d\n",m68k_dreg(&context->regs, 1));
+  	    return FALSE;
+	}
+      }
+   }
+
+    default:
+      JWLOG("ERROR!! aroshack_helper: unkown service %d\n",service);
+      return FALSE;
+  }
+
+}
