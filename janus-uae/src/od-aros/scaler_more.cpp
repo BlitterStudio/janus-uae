e61c58e97ac6e22959218d6986be8e0298a22139
o1i <o1i@b46322d7-8905-45b6-b196-3a62c27b8adb>
2011-07-22 07:43:23 +0000

    added scaler_more.cpp

---
+++ janus-uae/src/od-aros/scaler_more.cpp
@@ -0,0 +1,41 @@
+/************************************************************************ 
+ *
+ * UAE - The Un*x Amiga Emulator
+ *
+ * scaler_more
+ *
+ * Copyright 2011 Oliver Brunner - aros<at>oliver-brunner.de
+ *
+ * This file is part of Janus-UAE.
+ *
+ * Janus-UAE is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Janus-UAE is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Janus-UAE. If not, see <http://www.gnu.org/licenses/>.
+ *
+ * $Id$
+ *
+ ************************************************************************/
+
+#include "sysconfig.h"
+#include "sysdeps.h"
+
+#include "options.h"
+#include "filter.h"
+#include "custom.h"
+
+#include <math.h>
+
+uae_s32 tyhrgb[65536];
+uae_s32 tylrgb[65536];
+uae_s32 tcbrgb[65536];
+uae_s32 tcrrgb[65536];
+
