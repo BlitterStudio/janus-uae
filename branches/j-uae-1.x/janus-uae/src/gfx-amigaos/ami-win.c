95a449c8699a6c089eda9e7831644e005b43b4d3
o1i <o1i@b46322d7-8905-45b6-b196-3a62c27b8adb>
2012-04-30 08:51:51 +0000

    WA_ShapeRegion seems to be gone. Use WA_Shape instead.

--- branches/j-uae-1.x/janus-uae/src/gfx-amigaos/ami-win.c
+++ branches/j-uae-1.x/janus-uae/src/gfx-amigaos/ami-win.c
@@ -1190,7 +1190,7 @@ static int setup_publicscreen(void) {
               WA_AutoAdjust,   TRUE,
               WA_InnerWidth,   gfxvidinfo.width,
               WA_InnerHeight,  gfxvidinfo.height,
-              WA_ShapeRegion,  shape,
+              WA_Shape,        shape,
               WA_PubScreen,    (ULONG)S,
               WA_Zoom,         (ULONG)ZoomArray,
               WA_IDCMP,        IDCMP_MOUSEBUTTONS | IDCMP_RAWKEY
