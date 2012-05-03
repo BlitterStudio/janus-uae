8a1cdcd2c49f676b3edc7eebde429ba2a8f47755
o1i <o1i@b46322d7-8905-45b6-b196-3a62c27b8adb>
2012-05-03 11:36:58 +0000

    forgotten, too

---
+++ janus-uae/src/od-aros/picasso96_aros.cpp
@@ -0,0 +1,62 @@
+/************************************************************************ 
+ *
+ * Picasso96 Support Module
+ *
+ * Copyright 1997-2001 Brian King <Brian_King@Mitel.com, Brian_King@Cloanto.com>
+ * Copyright 2000-2001 Bernd Roesch
+ * Copyright 2003-2005 Richard Drummond
+ * Copyright 2009-2011 Oliver Brunner <aros<at>oliver-brunner.de>
+ *
+ * This file is part of Janus-UAE.
+ *
+ * Janus-UAE is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Janus-UAE is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Janus-UAE. If not, see <http://www.gnu.org/licenses/>.
+ *
+ * $Id$
+ *
+ ************************************************************************
+ *
+ * Theory of operation:
+ * 
+ * On the Amiga side, a Picasso card consists mainly of a memory area that
+ * contains the frame buffer.  On the UAE side, we allocate a block of memory
+ * that will hold the frame buffer.  This block is in normal memory, it is
+ * never directly on the graphics card.  All graphics operations, which are
+ * mainly reads and writes into this block and a few basic operations like
+ * filling a rectangle, operate on this block of memory.
+ * Since the memory is not on the graphics card, some work must be done to
+ * synchronize the display with the data in the Picasso frame buffer.  There
+ * are various ways to do this.  One possibility is to allocate a second
+ * buffer of the same size, and perform all write operations twice.  Since
+ * we never read from the second buffer, it can actually be placed in video
+ * memory.  The X11 driver could be made to use the Picasso frame buffer as
+ * the data buffer of an XImage, which could then be XPutImage()d from time
+ * to time.  Another possibility is to translate all Picasso accesses into
+ * Xlib (or GDI, or whatever your graphics system is) calls.  This possibility
+ * is a bit tricky, since there is a risk of generating very many single pixel
+ * accesses which may be rather slow.
+ *
+ * TODO:
+ * - we want to add a manual switch to override SetSwitch for hardware banging
+ *   programs started from a Picasso workbench.
+ */
+#include "sysconfig.h"
+#include "sysdeps.h"
+
+#include "picasso96_aros.h"
+
+uae_u32 p96_rgbx16[65536];
+
+bool have_done_picasso = 1; /* For the JIT compiler */
+
+//struct picasso96_state_struct picasso96_state;
