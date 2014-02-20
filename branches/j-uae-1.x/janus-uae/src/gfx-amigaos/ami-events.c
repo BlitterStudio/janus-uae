82bf88e90d9603b948e3b7dbba59c2722398fd3f
o1i <o1i@b46322d7-8905-45b6-b196-3a62c27b8adb>
2014-02-20 16:13:29 +0000

    retab

--- branches/j-uae-1.x/janus-uae/src/gfx-amigaos/ami-events.c
+++ branches/j-uae-1.x/janus-uae/src/gfx-amigaos/ami-events.c
@@ -151,9 +151,9 @@ void handle_events_W(struct Window *W, BOOL customscreen) {
      * is lost when system() is called.
      */
     if (SetSignal (0L, SIGBREAKF_CTRL_C | SIGBREAKF_CTRL_D) &
-		(SIGBREAKF_CTRL_C|SIGBREAKF_CTRL_D)) {
+                (SIGBREAKF_CTRL_C|SIGBREAKF_CTRL_D)) {
       kprintf("SIGBREAKF_CTRL_C|SIGBREAKF_CTRL_D: activate_debugger !!\n");
-	activate_debugger ();
+        activate_debugger ();
     }
 #endif
 
@@ -161,30 +161,30 @@ void handle_events_W(struct Window *W, BOOL customscreen) {
     if (screen_is_picasso) {
         int i;
 
-	JWLOG("handle_events->screen_is_picasso (aroswin %lx)\n", W);
+        JWLOG("handle_events->screen_is_picasso (aroswin %lx)\n", W);
 
         picasso_invalid_lines[picasso_invalid_end+1] = 0;
         picasso_invalid_lines[picasso_invalid_end+2] = 1;
 
-    	//o1i_Display_Update(0,0);
+        //o1i_Display_Update(0,0);
 
-	/* o1i TODO: remove loops?*/
-	for (i = picasso_invalid_start; i < picasso_invalid_end;)
-	{
-	    int start = i;
+        /* o1i TODO: remove loops?*/
+        for (i = picasso_invalid_start; i < picasso_invalid_end;)
+        {
+            int start = i;
 
             for (;picasso_invalid_lines[i]; i++)
-	        picasso_invalid_lines[i] = 0;
+                picasso_invalid_lines[i] = 0;
 
             //DoMethod(uaedisplay, MUIM_UAEDisplay_Update, start, i);
-	    //JWLOG("MUIM_UAEDisplay_Update TODO\n");
-	    o1i_Display_Update(start,i);
+            //JWLOG("MUIM_UAEDisplay_Update TODO\n");
+            o1i_Display_Update(start,i);
 
-	    for (; !picasso_invalid_lines[i]; i++);
-	}
+            for (; !picasso_invalid_lines[i]; i++);
+        }
 
-	picasso_invalid_start = picasso_vidinfo.height + 1;
-	picasso_invalid_end   = -1;
+        picasso_invalid_start = picasso_vidinfo.height + 1;
+        picasso_invalid_end   = -1;
     }
     else {
       JWLOG("screen_is_picasso == FALSE (aroswin %lx)\n", W);
@@ -196,44 +196,44 @@ void handle_events_W(struct Window *W, BOOL customscreen) {
     }
     else {
       if(!uae_main_window_closed) {
-	obtain_W();
-	JWLOG("GetMsg(W %lx->UserPort %lx)\n", W, W->UserPort);
+        obtain_W();
+        JWLOG("GetMsg(W %lx->UserPort %lx)\n", W, W->UserPort);
 
-	while (!uae_main_window_closed && (msg = (struct IntuiMessage*) GetMsg(W->UserPort))) {
-	  class     = msg->Class;
-	  code      = msg->Code;
-	  dmx       = msg->MouseX;
-	  dmy       = msg->MouseY;
-	  mx        = msg->IDCMPWindow->MouseX; // Absolute pointer coordinates
-	  my        = msg->IDCMPWindow->MouseY; // relative to the window
-	  qualifier = msg->Qualifier;
+        while (!uae_main_window_closed && (msg = (struct IntuiMessage*) GetMsg(W->UserPort))) {
+          class     = msg->Class;
+          code      = msg->Code;
+          dmx       = msg->MouseX;
+          dmy       = msg->MouseY;
+          mx        = msg->IDCMPWindow->MouseX; // Absolute pointer coordinates
+          my        = msg->IDCMPWindow->MouseY; // relative to the window
+          qualifier = msg->Qualifier;
 
-	  ReplyMsg ((struct Message*)msg);
+          ReplyMsg ((struct Message*)msg);
 
-	  JWLOG("W: %lx (%s)\n",W, W->Title);
-	  JWLOG("ami-win.c: handle_input(win %lx)\n", W);
+          JWLOG("W: %lx (%s)\n",W, W->Title);
+          JWLOG("ami-win.c: handle_input(win %lx)\n", W);
 
-	  switch (class) {
-	      case IDCMP_NEWSIZE:
-		  do_inhibit_frame ((W->Flags & WFLG_ZOOMED) ? 1 : 0);
-		  break;
+          switch (class) {
+              case IDCMP_NEWSIZE:
+                  do_inhibit_frame ((W->Flags & WFLG_ZOOMED) ? 1 : 0);
+                  break;
 
-	      case IDCMP_REFRESHWINDOW:
-		  if (use_delta_buffer) {
-		      /* hack: this forces refresh */
-		      uae_u8 *ptr = oldpixbuf;
-		      int i, len = gfxvidinfo.width;
-		      len *= gfxvidinfo.pixbytes;
-		      for (i=0; i < currprefs.gfx_height_win; ++i) {
-			  ptr[00000] ^= 255;
-			  ptr[len-1] ^= 255;
-			  ptr += gfxvidinfo.rowbytes;
-		      }
-		  }
-		  BeginRefresh (W);
-		  flush_block (0, currprefs.gfx_height_win - 1);
-		  EndRefresh (W, TRUE);
-		  break;
+              case IDCMP_REFRESHWINDOW:
+                  if (use_delta_buffer) {
+                      /* hack: this forces refresh */
+                      uae_u8 *ptr = oldpixbuf;
+                      int i, len = gfxvidinfo.width;
+                      len *= gfxvidinfo.pixbytes;
+                      for (i=0; i < currprefs.gfx_height_win; ++i) {
+                          ptr[00000] ^= 255;
+                          ptr[len-1] ^= 255;
+                          ptr += gfxvidinfo.rowbytes;
+                      }
+                  }
+                  BeginRefresh (W);
+                  flush_block (0, currprefs.gfx_height_win - 1);
+                  EndRefresh (W, TRUE);
+                  break;
 
     case IDCMP_CLOSEWINDOW:
       if(really_quit(W)) {
@@ -241,131 +241,131 @@ void handle_events_W(struct Window *W, BOOL customscreen) {
       }
       break;
 
-	      case IDCMP_RAWKEY: {
-		  int keycode = code & 127;
-		  int state   = code & 128 ? 0 : 1;
-		  int ievent;
+              case IDCMP_RAWKEY: {
+                  int keycode = code & 127;
+                  int state   = code & 128 ? 0 : 1;
+                  int ievent;
 
-		  if ((qualifier & IEQUALIFIER_REPEAT) == 0) {
-		      /* We just want key up/down events - not repeats */
-		      if ((ievent = match_hotkey_sequence (keycode, state)))
-			  handle_hotkey_event (ievent, state);
-		      else
-			  inputdevice_do_keyboard (keycode, state);
-		  }
-		  break;
-	       }
+                  if ((qualifier & IEQUALIFIER_REPEAT) == 0) {
+                      /* We just want key up/down events - not repeats */
+                      if ((ievent = match_hotkey_sequence (keycode, state)))
+                          handle_hotkey_event (ievent, state);
+                      else
+                          inputdevice_do_keyboard (keycode, state);
+                  }
+                  break;
+               }
 
-	      case IDCMP_MOUSEMOVE:
-		/* classic mouse move, if either option is disabled or janusd is not (yet) running */
-		if( ((!changed_prefs.jmouse) || (!aos3_task) ) && (!uae_main_window_closed)) {
-		  JWLOG("classic mouse move enabled\n");
-		  setmousestate (0, 0, dmx, 0);
-		  setmousestate (0, 1, dmy, 0);
+              case IDCMP_MOUSEMOVE:
+                /* classic mouse move, if either option is disabled or janusd is not (yet) running */
+                if( ((!changed_prefs.jmouse) || (!aos3_task) ) && (!uae_main_window_closed)) {
+                  JWLOG("classic mouse move enabled\n");
+                  setmousestate (0, 0, dmx, 0);
+                  setmousestate (0, 1, dmy, 0);
 
-		  if (usepub) {
-		    update_pointer(W, mx, my);
+                  if (usepub) {
+                    update_pointer(W, mx, my);
 #if 0
-		      POINTER_STATE new_state = get_pointer_state (W, mx, my);
-		      if (new_state != pointer_state) {
-			  pointer_state = new_state;
-			  if (pointer_state == INSIDE_WINDOW)
-			      hide_pointer (W);
-			  else
-			      show_pointer (W);
-		      }
+                      POINTER_STATE new_state = get_pointer_state (W, mx, my);
+                      if (new_state != pointer_state) {
+                          pointer_state = new_state;
+                          if (pointer_state == INSIDE_WINDOW)
+                              hide_pointer (W);
+                          else
+                              show_pointer (W);
+                      }
 #endif
-		  }
-		}
-		else {
-		  JWLOG("classic mouse move disabled ((%d || %d) && %d)\n", (!changed_prefs.jmouse), (!aos3_task), (!uae_main_window_closed));
-		}
-		break;
+                  }
+                }
+                else {
+                  JWLOG("classic mouse move disabled ((%d || %d) && %d)\n", (!changed_prefs.jmouse), (!aos3_task), (!uae_main_window_closed));
+                }
+                break;
 
-	    case IDCMP_MOUSEBUTTONS:
-		if (code == SELECTDOWN) 
-		{
-			setmousebuttonstate (0, 0, 1);
-			gMouseState |= 8;
-		}
-		if (code == SELECTUP)   
-		{
-			setmousebuttonstate (0, 0, 0);
-			gMouseState &= ~8;
-		}
+            case IDCMP_MOUSEBUTTONS:
+                if (code == SELECTDOWN) 
+                {
+                        setmousebuttonstate (0, 0, 1);
+                        gMouseState |= 8;
+                }
+                if (code == SELECTUP)   
+                {
+                        setmousebuttonstate (0, 0, 0);
+                        gMouseState &= ~8;
+                }
         if (code == MIDDLEDOWN) 
-		{
-			setmousebuttonstate (0, 2, 1);
-			gMouseState |= 4;
-		}
-		if (code == MIDDLEUP)   
-		{
-			setmousebuttonstate (0, 2, 0);
-			gMouseState &= ~4;
-		}
-		if (code == MENUDOWN)
-		{
-			setmousebuttonstate (0, 1, 1);
-			gMouseState |= 2;
-		}
-		if (code == MENUUP)     
-		{
-			setmousebuttonstate (0, 1, 0);
-			gMouseState &= ~2;
-		}
-		break;
+                {
+                        setmousebuttonstate (0, 2, 1);
+                        gMouseState |= 4;
+                }
+                if (code == MIDDLEUP)   
+                {
+                        setmousebuttonstate (0, 2, 0);
+                        gMouseState &= ~4;
+                }
+                if (code == MENUDOWN)
+                {
+                        setmousebuttonstate (0, 1, 1);
+                        gMouseState |= 2;
+                }
+                if (code == MENUUP)     
+                {
+                        setmousebuttonstate (0, 1, 0);
+                        gMouseState &= ~2;
+                }
+                break;
 
-	      /* Those 2 could be of some use later. */
-	      case IDCMP_DISKINSERTED:
-		  /*printf("diskinserted(%d)\n",code);*/
-		  break;
+              /* Those 2 could be of some use later. */
+              case IDCMP_DISKINSERTED:
+                  /*printf("diskinserted(%d)\n",code);*/
+                  break;
 
-	      case IDCMP_DISKREMOVED:
-		  /*printf("diskremoved(%d)\n",code);*/
-		  break;
+              case IDCMP_DISKREMOVED:
+                  /*printf("diskremoved(%d)\n",code);*/
+                  break;
 
-	      case IDCMP_ACTIVEWINDOW:
-		  /* When window regains focus (presumably after losing focus at some
-		   * point) UAE needs to know any keys that have changed state in between.
-		   * A simple fix is just to tell UAE that all keys have been released.
-		   * This avoids keys appearing to be "stuck" down.
-		   */
-		  JWLOG("IDCMP_ACTIVEWINDOW(%lx)\n", W);
-		  inputdevice_acquire ();
-		  inputdevice_release_all_keys ();
-		  reset_hotkeys ();
-		  copy_clipboard_to_amigaos();
-		  break;
+              case IDCMP_ACTIVEWINDOW:
+                  /* When window regains focus (presumably after losing focus at some
+                   * point) UAE needs to know any keys that have changed state in between.
+                   * A simple fix is just to tell UAE that all keys have been released.
+                   * This avoids keys appearing to be "stuck" down.
+                   */
+                  JWLOG("IDCMP_ACTIVEWINDOW(%lx)\n", W);
+                  inputdevice_acquire ();
+                  inputdevice_release_all_keys ();
+                  reset_hotkeys ();
+                  copy_clipboard_to_amigaos();
+                  break;
 
-	      case IDCMP_INACTIVEWINDOW:
-		  JWLOG("IDCMP_INACTIVEWINDOW\n");
-		  JWLOG("IntuitionBase->ActiveWindow: %lx (%s)\n",IntuitionBase->ActiveWindow, IntuitionBase->ActiveWindow->Title);
-		  copy_clipboard_to_aros();
-		  inputdevice_unacquire ();
-		  break;
+              case IDCMP_INACTIVEWINDOW:
+                  JWLOG("IDCMP_INACTIVEWINDOW\n");
+                  JWLOG("IntuitionBase->ActiveWindow: %lx (%s)\n",IntuitionBase->ActiveWindow, IntuitionBase->ActiveWindow->Title);
+                  copy_clipboard_to_aros();
+                  inputdevice_unacquire ();
+                  break;
 
-	      case IDCMP_INTUITICKS:
+              case IDCMP_INTUITICKS:
 #ifdef __amigaos4__ 
-		  grabTicks--;
-		  if (grabTicks < 0) {
-		      grabTicks = GRAB_TIMEOUT;
-		      #ifdef __amigaos4__ 
-			  if (mouseGrabbed)
-			      grab_pointer (W);
-		      #endif
-		  }
+                  grabTicks--;
+                  if (grabTicks < 0) {
+                      grabTicks = GRAB_TIMEOUT;
+                      #ifdef __amigaos4__ 
+                          if (mouseGrabbed)
+                              grab_pointer (W);
+                      #endif
+                  }
 #endif
-		  break;
+                  break;
 
-	      default:
-		  write_log ("Unknown event class: %x\n", class);
-		  JWLOG("Unknown event class: %x\n", class);
-		  break;
-	  }
-	}
+              default:
+                  write_log ("Unknown event class: %x\n", class);
+                  JWLOG("Unknown event class: %x\n", class);
+                  break;
+          }
+        }
 
-	JWLOG("GetMsg(W->UserPort %lx) done\n", W->UserPort);
-	release_W();
+        JWLOG("GetMsg(W->UserPort %lx) done\n", W->UserPort);
+        release_W();
       }
     }
 
@@ -411,11 +411,11 @@ static int keyhack (int scancode, int pressed, int num)
 static void read_kb (void)
 {
 #ifdef CATWEASEL
-		uae_u8 kc;
-		if (catweasel_read_keyboard (&kc))
-		{
-			inputdevice_do_keyboard (kc & 0x7f, !(kc & 0x80));
-		}
+                uae_u8 kc;
+                if (catweasel_read_keyboard (&kc))
+                {
+                        inputdevice_do_keyboard (kc & 0x7f, !(kc & 0x80));
+                }
 #endif
 }
 
