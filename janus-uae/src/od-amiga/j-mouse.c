00f8cae398d0e76597ca18d6e77f770356146c59
o1i <o1i@b46322d7-8905-45b6-b196-3a62c27b8adb>
2014-04-15 14:05:55 +0000

    switch off debug

--- janus-uae/src/od-amiga/j-mouse.c
+++ janus-uae/src/od-amiga/j-mouse.c
@@ -21,7 +21,7 @@
  *
  ************************************************************************/
 
-#define JWTRACING_ENABLED 1
+//#define JWTRACING_ENABLED 1
 #include "j.h"
 
 //int coord_native_to_amiga_x (int x);
