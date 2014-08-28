c6ef837c4c759d59ff79779d8f07df40069ae53d
o1i <o1i@b46322d7-8905-45b6-b196-3a62c27b8adb>
2014-08-28 07:45:00 +0000

    clean this tag (tree structure)

--- tags/win-uae-2.3.1/win-uae-2.3.1/janus-uae/src/include/enforcer.h
+++
@@ -1,9 +0,0 @@
-#ifndef UAE__ENFORCER_H
-#define UAE__ENFORCER_H
-
-int enforcer_enable(int);
-int enforcer_disable(void);
-
-extern int enforcermode;
-
-#endif
--- tags/win-uae-2.3.1/win-uae-2.3.1/janus-uae/src/include/sampler.h
+++
@@ -1,6 +0,0 @@
-
-extern uae_u8 sampler_getsample (int);
-extern int sampler_init (void);
-extern void sampler_free (void);
-extern void sampler_vsync (void);
-
