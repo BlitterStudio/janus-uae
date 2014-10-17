922ca8617007f53ab5cceb2bf81fcf52e495b5c6
o1i <o1i@b46322d7-8905-45b6-b196-3a62c27b8adb>
2014-10-17 09:16:02 +0000

    first try..

---
+++ janus-uae/src/od-aros/gui/gui.h
@@ -0,0 +1,28 @@
+#ifndef __GUI_H__
+#define __GUI_H__
+
+
+typedef struct Element {
+  BOOL exists;
+  Object *obj;
+  ULONG windows_type;
+  ULONG x,y,w,h;
+  const char *text;
+  ULONG options;
+} ;
+
+enum {
+  MA_src
+};
+
+#define GETDATA struct Data *data = (struct Data *)INST_DATA(cl, obj)
+#define SETUPHOOK(x, func, data) { struct Hook *h = (x); h->h_Entry = (HOOKFUNC)&func; h->h_Data = (APTR)data; }
+#define BEGINMTABLE AROS_UFH3(static ULONG,mDispatcher,AROS_UFHA(struct IClass*, cl, a0), AROS_UFHA(APTR, obj, a2), AROS_UFHA(Msg, msg, a1))
+#define ENDMTABLE return DoSuperMethodA(cl, (Object *) obj, msg); }
+
+int init_class(void);
+void delete_class(void);
+
+extern struct MUI_CustomClass *CL_Fixed;
+
+#endif
