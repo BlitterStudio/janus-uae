28f16aec3911eeed7f1f2913199f286d7546db1b
o1i <o1i@b46322d7-8905-45b6-b196-3a62c27b8adb>
2015-06-23 15:36:58 +0000

    trying to make better use of Listtrees. Still WIP

--- trunk/janus-uae/src/od-aros/gui/mui_tree.cpp
+++ trunk/janus-uae/src/od-aros/gui/mui_tree.cpp
@@ -11,9 +11,12 @@
 #include <clib/alib_protos.h>
 
 #include <graphics/gfxbase.h>
+#include <mui/NList_mcc.h>
 #include <mui/NListtree_mcc.h>
 #include <mui/NListview_mcc.h>
+#include <mui/NFloattext_mcc.h>
 
+#include <SDI/SDI_hook.h>
 
 #define OLI_DEBUG
 #include "sysconfig.h"
@@ -23,6 +26,7 @@
 #include "gui.h"
 #include "winnt.h"
 #include "mui_data.h"
+#include "gui_mui.h"
 
 /* Deletes all items from a tree-view control. 
  * WARNING: nIDDlgItem is not in the Windows API!
@@ -67,8 +71,10 @@ HTREEITEM TreeView_InsertItem(HWND elem, int item, LPTVINSERTSTRUCT lpis) {
   }
 
   DebOut("elem[i].obj: %lx\n", elem[i].obj);
+  DebOut("lpis: %lx\n", lpis);
 
-  DoMethod(elem[i].obj, MUIM_NListtree_Insert, lpis->itemex.pszText, NULL,
+  /* save config (lParam) in UserData */
+  DoMethod(elem[i].obj, MUIM_NListtree_Insert, lpis->itemex.pszText, (IPTR) lpis->itemex.lParam,
                         MUIV_NListtree_Insert_ListNode_Root,
                         MUIV_NListtree_Insert_PrevNode_Tail,
                         MUIV_NListtree_Insert_Flag_Active);
@@ -92,4 +98,120 @@ BOOL TreeView_GetItem(HWND elem, int item, APTR pitem_void) {
   LPTVITEM pitem=(LPTVITEM) pitem_void; /* gcc does no automatic cast here, as MS C++ does */
 
   TODO();
+}
+
+/**********************************************************************************************/
+
+#if 0
+#warning THIS IS WRONG!! Defined in win32gui.cpp!!
+struct ConfigStruct {
+	TCHAR Name[MAX_DPATH];
+	TCHAR Path[MAX_DPATH];
+	TCHAR Fullpath[MAX_DPATH];
+	TCHAR HostLink[MAX_DPATH];
+	TCHAR HardwareLink[MAX_DPATH];
+};
+
+struct TreeLine {
+  TCHAR name[MAX_DPATH];
+  struct ConfigStruct *config;
+};
+
+HOOKPROTONHNO(TreeView_DisplayHookFunction, void, struct MUIP_NListtree_DisplayMessage *ndm) {
+
+  struct TreeLine *line=(struct TreeLine *) ndm->TreeNode->tn_User;
+
+  DebOut("line: %lx\n", line);
+
+  if(line) {
+    DebOut("line->: %s\n", line->name);
+    *ndm->Array++=line->name;
+    //ndm->strings[0]="FOO";
+    //ndm->preparses[0]=NULL;
+  }
+  else {
+    /* we don't have a header bar! */
+    DebOut("ERROR!?\n");
+  }
+}
+
+HOOKPROTONHNO(TreeView_ConstructHookFunction, APTR, struct MUIP_NListtree_ConstructMessage *ncm) {
+  DebOut("ncm: %lx\n", ncm);
+
+  struct TreeLine *line=(struct TreeLine *) AllocVec(sizeof(struct TreeLine), 0);
+
+  TVINSERTSTRUCT *is=(TVINSERTSTRUCT *) ncm->UserData;
+  DebOut("is: %lx\n", is);
+  DebOut("ncm->name: %s\n", ncm->Name);
+
+  if(line && is) {
+    line->config=(struct ConfigStruct *) AllocVec(sizeof(struct ConfigStruct), 0);
+
+    if(is->itemex.pszText) {
+      DebOut("is->itemex.pszText: %s\n", is->itemex.pszText);
+      strncpy(line->name, is->itemex.pszText, MAX_DPATH-1);
+    }
+
+    if(is->itemex.lParam) {
+      memcpy((void *) line->config, (const  void *) is->itemex.lParam, sizeof(struct ConfigStruct));
+    }
+  }
+  else {
+    DebOut("NOT ENOUGH MEMORY!\n");
+  }
+
+  DebOut("return: %lx\n", line);
+
+  return line;
+}
+
+HOOKPROTONHNO(TreeView_DestructHook, void, struct NList_DestructMessage *ndm) {
+
+  struct TreeLine *line;
+
+  DebOut("ndm: %lx\n", ndm);
+
+  if (ndm->entry) {
+    line=(struct TreeLine *) ndm->entry;
+    if(line->config) {
+      FreeVec((void *) line->config);
+    }
+    FreeVec((void *) line);
+  }
+
+}
+#endif
+
+Object *new_tree(ULONG i, struct Hook *construct_hook, struct Hook *destruct_hook, struct Hook *display_hook) {
+
+  Object *tree=NULL;
+
+#if 0
+  construct_hook->h_Entry=(APTR) TreeView_ConstructHookFunction;
+  construct_hook->h_Data=NULL;
+  destruct_hook->h_Entry=(APTR) TreeView_DestructHook;
+  destruct_hook->h_Data=NULL;
+  display_hook->h_Entry=(APTR) TreeView_DisplayHookFunction;
+  display_hook->h_Data=NULL;
+#endif
+
+  tree=HGroup,
+          MUIA_UserData, i,
+          Child, (NListviewObject,
+            MUIA_UserData, i,
+            MUIA_NListview_NList, (NListtreeObject,
+                MUIA_UserData, i,
+                ReadListFrame,
+                MUIA_CycleChain, TRUE,
+                MUIA_NList_Title, FALSE,
+                //MUIA_NListtree_DisplayHook,   display_hook,
+                //MUIA_NList_ConstructHook2, construct_hook,
+                //MUIA_NListtree_DestructHook,  destruct_hook,
+                MUIA_NListtree_DragDropSort,  FALSE,
+                //MUIA_NListtree_ShowTree, FALSE,
+              End),
+            End),
+          End;
+
+  return tree;
 }
