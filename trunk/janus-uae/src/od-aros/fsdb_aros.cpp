ad37c8f170e1a102a2b36f961b6dab57d6f43616
o1i <o1i@b46322d7-8905-45b6-b196-3a62c27b8adb>
2015-07-06 14:02:25 +0000

    make rename work over filesystem boundaries

--- trunk/janus-uae/src/od-aros/fsdb_aros.cpp
+++ trunk/janus-uae/src/od-aros/fsdb_aros.cpp
@@ -662,6 +662,7 @@ int my_rmdir (const TCHAR *name) {
 int my_rename (const TCHAR *oldname, const TCHAR *newname) {
 
   LONG res;
+	char cmd[255];
 
   DebOut("oldname: %s newname %s\n", oldname, newname);
 
@@ -669,6 +670,22 @@ int my_rename (const TCHAR *oldname, const TCHAR *newname) {
 
   if(res==DOSTRUE) {
     return 0;
+  }
+
+  /* ugly hack, as windows rename can move over filesystem borders,
+	 * but AROS rename can't. And there seems to be no simple
+	 * API to move a file across filesystem borders...
+	 * So we use the shell and hope for the best..
+	 */
+	DebOut("rename failed, try shell copy..\n");
+	sprintf(cmd, "c:copy from %s to %s MOVE QUIET >NIL:", oldname, newname);
+
+	DebOut("execute: %s\n", cmd);
+	Execute(cmd, NULL, NULL);
+
+	if(fsdb_exists(newname)) {
+    /* there is a file, pray, it is the one we copied ;) */
+		return 0;
   }
 
   SetLastError(IoErr());
