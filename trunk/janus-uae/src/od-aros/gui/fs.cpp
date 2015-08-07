a11a22e8f2d11694170fdba937a29170b71f9fd9
o1i <o1i@b46322d7-8905-45b6-b196-3a62c27b8adb>
2015-08-07 11:41:12 +0000

    Windows File API, at least enough for hardfile creation

--- trunk/janus-uae/src/od-aros/gui/fs.cpp
+++ trunk/janus-uae/src/od-aros/gui/fs.cpp
@@ -1,10 +1,13 @@
+#define JUAE_DEBUG
+
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
 
 #include <exec/types.h>
-#include <libraries/mui.h>
  
 #include <proto/exec.h>
-#include <proto/intuition.h>
-#include <proto/utility.h>
+#include <proto/dos.h>
 #include <clib/alib_protos.h>
 
 #include "sysconfig.h"
@@ -15,22 +18,112 @@
 #include "fs.h"
 
 
+/*
+ * Creates or opens a file or I/O device. 
+ */
 HANDLE CreateFile( LPCTSTR lpFileName, DWORD dwDesiredAccess, DWORD dwShareMode, LPSECURITY_ATTRIBUTES lpSecurityAttributes, DWORD dwCreationDisposition, DWORD dwFlagsAndAttributes, HANDLE hTemplateFile) {
-  TODO();
+
+  FILE *file=NULL;
+
+  DebOut("lpFileName: %s\n", lpFileName);
+
+  if(dwDesiredAccess!=GENERIC_WRITE || dwCreationDisposition!=CREATE_ALWAYS || dwFlagsAndAttributes!=FILE_ATTRIBUTE_NORMAL) {
+    TODO();
+  }
+
+  file=fopen(lpFileName, "w" );
+  if(file==NULL) {
+    DebOut("ERROR opening %s for writing\n", lpFileName);
+    return 0;
+  }
+
+  return file;
 }
 
+/*
+ * Writes data to the specified file or input/output (I/O) device.
+ */
 BOOL WriteFile(HANDLE hFile, LPCVOID lpBuffer, DWORD nNumberOfBytesToWrite, LPDWORD lpNumberOfBytesWritten, LPOVERLAPPED lpOverlapped) {
-  TODO();
+
+  LONG written;
+
+  DebOut("write %d bytes to file handle %lx\n", nNumberOfBytesToWrite, hFile);
+
+  written=write((IPTR) hFile, lpBuffer, nNumberOfBytesToWrite);
+
+  if(written==-1) {
+    return FALSE;
+  }
+
+  *lpNumberOfBytesWritten=written;
+
+  return TRUE;
 }
 
+/* not needed. Just flushes file to disk */
 BOOL SetEndOfFile(HANDLE hFile) {
-  TODO();
+  ;
 }
+
+/*
+ * Moves the file pointer of the specified file.
+ */
+
+#define SPACE_BLOCK 1024*1024
 
 DWORD SetFilePointer(HANDLE hFile, LONG lDistanceToMove, PLONG lpDistanceToMoveHigh, DWORD  dwMoveMethod) {
-  TODO();
+
+  LONG mode;
+  LONG res;
+  IPTR t;
+  IPTR act_size;
+  IPTR start_pos;
+  void *ptr;
+  LONG remaining;
+
+  if(lpDistanceToMoveHigh) {
+    DebOut("ERROR: no 64bit support so far!\n");
+    return 0;
+  }
+
+  DebOut("hFile %lx, lDistanceToMove: %d\n", lDistanceToMove);
+
+  switch(dwMoveMethod) {
+    case FILE_BEGIN:
+      break;
+    default:
+      DebOut("dwMoveMethod: %d not supported!\n", dwMoveMethod);
+      TODO();
+      return 0;
+  }
+
+  start_pos=ftell((FILE *) hFile);
+  act_size=fseek((FILE *) hFile, 0, SEEK_END);
+  DebOut("old size: %d\n", act_size);
+
+  if(act_size!=lDistanceToMove) {
+    ptr=calloc(1, SPACE_BLOCK);
+    if(ptr) {
+      remaining=lDistanceToMove;
+      while(remaining > SPACE_BLOCK) {
+        fwrite(ptr, SPACE_BLOCK, 1, (FILE *) hFile);
+        remaining=remaining-SPACE_BLOCK;
+      }
+      fwrite(ptr, remaining, 1, (FILE *) hFile);
+    }
+    free(ptr);
+  }
+
+  rewind((FILE *)hFile);
+  act_size=fseek((FILE *) hFile, 0, SEEK_END);
+  DebOut("new size: %d\n", act_size);
+
+  return 0;
 }
 
-BOOL CloseHandle(HANDLE hObject) {
-  TODO();
+BOOL CloseHandle(HANDLE hFile) {
+
+  fclose((FILE *) hFile);
+
+  return TRUE;
 }
