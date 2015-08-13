65d10e4647b2088e42472ea8cc7a6bf132804d9d
o1i <o1i@b46322d7-8905-45b6-b196-3a62c27b8adb>
2015-08-13 09:18:39 +0000

    Add host drives on startup .. WIP

--- janus-uae/src/od-aros/filesys.cpp
+++ janus-uae/src/od-aros/filesys.cpp
@@ -1,10 +1,59 @@
 #include <stdio.h>
 
-#include "sysconfig.h"
-#include "sysdeps.h"
+/*
+ * WARNING:
+ *
+ * this file is *included* from ../filesys.cpp
+ *
+ * So make sure, it really gets built new, if you do changes..
+ *
+ * No, this was not my idea..
+ */
 
-#include "options.h"
+#define JUAE_DEBUG 
 
 void filesys_addexternals (void) {
-  DebOut("filesys_addexternals: automount of removeable devices not yet implemented!\n");
+
+  struct DosList *dl;
+  BPTR lock;
+
+  if (!currprefs.win32_automount_cddrives && !currprefs.win32_automount_netdrives
+    && !currprefs.win32_automount_drives && !currprefs.win32_automount_removabledrives) {
+
+#warning remove me!
+#if 0
+    return;
+#endif
+  }
+
+  dl = LockDosList(LDF_READ|LDF_VOLUMES);
+  DebOut("dl: %lx\n", dl);
+
+  if(!dl) {
+    DebOut("ERROR: LockDosList failed!\n");
+    return;
+  }
+
+  struct DosList *tdl=dl;
+
+  while ((tdl = NextDosEntry(tdl, LDF_VOLUMES))) {
+
+    struct uaedev_config_info ci = { 0 };
+
+    DebOut("Volume: %s\n", tdl->dol_Name);
+    lock=Lock(tdl->dol_Name, SHARED_LOCK);
+    DebOut("lock: %lx\n", lock);
+    if(!lock) continue;
+    UnLock(lock);
+
+    sprintf (ci.volname, "host_%s", tdl->dol_Name);
+    sprintf (ci.rootdir, "%s:", tdl->dol_Name);
+    DebOut("ci.volname: %s, ci.rootdir: %s\n", ci.volname, ci.rootdir);
+    ci.readonly = 1;
+    ci.bootpri = -20;
+    add_filesys_unit (&ci);
+  }
+
+  UnLockDosList(LDF_VOLUMES);
+
 }
