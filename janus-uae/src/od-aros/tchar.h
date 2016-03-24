2d5b2a459168fd2d8006eb2a3200e072015fa125
kalamatee <kalamatee@b46322d7-8905-45b6-b196-3a62c27b8adb>
2016-03-24 14:19:33 +0000

    small tidy up. fix indentation of defines so they are inline. move AROS specific define to the !UNICODE section, where it belongs.

--- janus-uae/src/od-aros/tchar.h
+++ janus-uae/src/od-aros/tchar.h
@@ -75,17 +75,17 @@ ii
  * Unicode functions
  */
 
-#define	_tprintf		wprintf
-#define	_ftprintf		fwprintf
-#define	_stprintf		swprintf
+#define	_tprintf        wprintf
+#define	_ftprintf       fwprintf
+#define	_stprintf       swprintf
 #define	_sntprintf	_snwprintf
-#define	_vtprintf		vwprintf
+#define	_vtprintf       vwprintf
 #define	_vftprintf	vfwprintf
 #define	_vstprintf	vswprintf
 #define	_vsntprintf	_vsnwprintf
 #define	_tscanf		wscanf
-#define	_ftscanf		fwscanf
-#define	_stscanf		swscanf
+#define	_ftscanf        fwscanf
+#define	_stscanf        swscanf
 #define	_fgettc		fgetwc
 #define	_fgettchar	_fgetwchar
 #define	_fgetts		fgetws
@@ -99,44 +99,44 @@ ii
 #define	_ungettc	ungetwc
 #define	_tcstod		wcstod
 #define	_tcstol		wcstol
-#define	_tcstoul		wcstoul
+#define	_tcstoul        wcstoul
 #define	_tcscat		wcscat
 #define	_tcschr		wcschr
 #define	_tcscmp		wcscmp
 #define	_tcscpy		wcscpy
 #define	_tcscspn	wcscspn
 #define	_tcslen		wcslen
-#define	_tcsncat		wcsncat
+#define	_tcsncat        wcsncat
 #define	_tcsncmp	wcsncmp
 #define	_tcsncpy	wcsncpy
-#define	_tcspbrk		wcspbrk
-#define	_tcsrchr		wcsrchr
+#define	_tcspbrk        wcspbrk
+#define	_tcsrchr        wcsrchr
 #define	_tcsspn		wcsspn
 #define	_tcsstr		wcsstr
 #define	_tcstok		wcstok
 #define	_tcsdup		_wcsdup
 #define	_tcsicmp	_wcsicmp
 #define	_tcsnicmp	_wcsnicmp
-#define	_tcsnset		_wcsnset
+#define	_tcsnset        _wcsnset
 #define	_tcsrev		_wcsrev
 #define	_tcsset		_wcsset
 #define	_tcslwr		_wcslwr
 #define	_tcsupr		_wcsupr
-#define	_tcsxfrm		wcsxfrm
-#define	_tcscoll		wcscoll
-#define	_tcsicoll		_wcsicoll
+#define	_tcsxfrm        wcsxfrm
+#define	_tcscoll        wcscoll
+#define	_tcsicoll       _wcsicoll
 #define	_istalpha	iswalpha
 #define	_istupper	iswupper
 #define	_istlower	iswlower
-#define	_istdigit		iswdigit
+#define	_istdigit       iswdigit
 #define	_istxdigit	iswxdigit
 #define	_istspace	iswspace
 #define	_istpunct	iswpunct
 #define	_istalnum	iswalnum
-#define	_istprint		iswprint
+#define	_istprint       iswprint
 #define	_istgraph	iswgraph
-#define	_istcntrl		iswcntrl
-#define	_istascii		iswascii
+#define	_istcntrl       iswcntrl
+#define	_istascii       iswascii
 #define	_totupper	towupper
 #define	_totlower	towlower
 #define	_ttoi		_wtoi
@@ -173,7 +173,7 @@ typedef char	TCHAR;
 #define	_vftprintf	vfprintf
 #define _vstprintf	vsprintf
 #define	_vsntprintf	vsnprintf
-#define _vsnprintf vsnprintf
+#define _vsnprintf      vsnprintf
 #define	_tscanf		scanf
 #define	_ftscanf	fscanf
 #define	_stscanf	sscanf
@@ -233,15 +233,12 @@ typedef char	TCHAR;
 #define	_ttoi		atoi
 #define _tcsftime	strftime
 
-
-#define fgetws fgets
-#define _tstoi64 atoi
+#define fgetws          fgets
+#ifdef __AROS__
+#define fputws          fputs
+#endif
+#define _tstoi64        atoi
 
 #endif	/* Not _UNICODE */
 
-#ifdef __AROS__
-#define fputws fputs
-#endif
-
 #endif	/* Not _TCHAR_H_ */
-
