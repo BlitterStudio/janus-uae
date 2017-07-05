312dd4fd28675692229a842042d808e26bb50d0c
o1i <o1i@b46322d7-8905-45b6-b196-3a62c27b8adb>
2017-07-05 09:08:36 +0000

    remove disabled code

--- janus-uae/src/od-aros/stubs.cpp
+++ janus-uae/src/od-aros/stubs.cpp
@@ -520,14 +520,6 @@ void master_sound_volume (int dir)
   TODO();
 }
 
-/*
-frame_time_t read_processor_time (void) 
-{
-    STUBD(bug("[JUAE:Stub] %s()\n", __PRETTY_FUNCTION__));
-  TODO();
-}
-*/
-
 static unsigned int serial_hsynchandler_count;
 /* seems to be necessary, calls ... => hsyncstuff ! */
 void serial_hsynchandler (void)
